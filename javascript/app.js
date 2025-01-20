55c655157bed25792b7501ee9b7fe774544c31cb
davidlkarlsson <david.karlsson@stud.sti.se>
2025-01-20 14:42:26 +0100

    second commit

--- javascript/app.js
+++ javascript/app.js
@@ -1,3 +1,16 @@
+const textarea = document.getElementById('responsive-textarea');
+
+window.addEventListener('resize', () => {
+    
+    if (window.innerWidth < 760) {
+        textarea.rows = 20;
+        textarea.cols = 30;
+    } else {
+        textarea.rows = 30;
+        textarea.cols = 40;
+    }
+});
+
 // Hämta element
 const hamburgerMenu = document.getElementById('hamburger-menu');
 const navMenu = document.getElementById('nav-mobile');
@@ -6,3 +19,6 @@ const navMenu = document.getElementById('nav-mobile');
 hamburgerMenu.addEventListener('click', () => {
     navMenu.classList.toggle('active'); // Visa/dölj menyn
 });
+
+
+
